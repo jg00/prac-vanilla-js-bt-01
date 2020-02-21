a2a1b95cf609f5162aedf53cfcdde2dcad198f66
Jack Gamboa <jgamboa929@gmail.com>
2020-02-21 13:06:45 -0600

    Meal by id and display start.

--- 08-meal-finder/script.js
+++ 08-meal-finder/script.js
@@ -15,7 +15,7 @@ function searchMeal(e) {
   // Get search term
   const term = search.value;
 
-  // Check for empth
+  // Check for empty
   if (term.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then(res => res.json())
@@ -31,7 +31,7 @@ function searchMeal(e) {
               meal => `
             <div class="meal">
               <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-              <div class="meal-info" data-mealid="${meal.idMeal}">
+              <div class="meal-info" data-mealID="${meal.idMeal}">
                 <h3>${meal.strMeal}</h3>
               </div>
             </div>
@@ -48,5 +48,71 @@ function searchMeal(e) {
   }
 }
 
+// Search meal details by id
+function getMealByID(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
+
+      console.log(meal);
+      addMealToDOM(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  // console.log(ingredients);
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+      </div>
+    </div>
+  `;
+}
+
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+
+mealsEl.addEventListener("click", e => {
+  // const mealInfo = e.path(); // e.path || e.composedPath() but e.path not exposed by all browsers
+
+  const mealInfo = e.composedPath().find(item => {
+    if (item.classList) {
+      return item.classList.contains("meal-info"); // classList returns a DOMTokenList and does not have a .indexOf() method.
+    }
+    return false;
+  });
+
+  console.log(mealInfo); //See results below
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealByID(mealID);
+  }
+});
+
+/* Reference only
+  console.log(mealInfo);
+  <div class="meal-info" data-mealid="52881">
+    <h3>Steak and Kidney Pie</h3>
+  </div>
+*/
