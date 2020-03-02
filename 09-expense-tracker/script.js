dd3fa01c133110e477889ccf68df941b738a9a36
Jack Gamboa <jgamboa929@gmail.com>
2020-03-02 16:37:12 -0600

    Display expense and income history and balances.

--- 09-expense-tracker/script.js
+++ 09-expense-tracker/script.js
@@ -1,1 +1,66 @@
+const balance = document.getElementById("balance");
+const money_plus = document.getElementById("money-plus");
+const money_minus = document.getElementById("money-minus");
+const list = document.getElementById("list"); // ul
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
 
+const dummyTransactions = [
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 }
+];
+
+let transactions = dummyTransactions;
+
+// Add each transaction to DOM list
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  const item = document.createElement("li");
+
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span>
+  <button class="delete-btn">x</button>`;
+
+  list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts
+    .reduce((acc, currentValue) => {
+      return acc + currentValue;
+    }, 0)
+    .toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => acc + item, 0)
+    .toFixed(2);
+
+  const expense = amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => acc + item, 0)
+    .toFixed(2);
+
+  balance.innerHTML = `${total}`;
+  money_plus.innerHTML = `${income}`;
+  money_minus.innerHTML = `${expense}`;
+}
+
+function init() {
+  list.innerHTML = "";
+  transactions.forEach(addTransactionDOM);
+
+  updateValues();
+}
+
+init();
